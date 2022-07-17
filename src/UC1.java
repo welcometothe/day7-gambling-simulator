338bf459dd5e897e333ea60415048eb4a9259bf7
unknown <vaibhavpatil100008@gmail.com>
2022-07-17 20:10:16 +0530

    bit every day code is done

---
+++ src/UC1.java
@@ -0,0 +1,8 @@
+public class UC1 {
+    public static void main (String[] args){
+        int stake_Amount = 100;
+        int bet = 1;
+        System.out.println("Amount of stake to start game of every day is + stake amount");
+        System.out.println("Amount to bet after every game " +  bet);
+    }
+}
